15b50386d8046390d4aa3b22b22b3d43bd4961c9
jakub <jakub@localhost.localdomain>
2026-03-26 00:31:44 +0100

    Refactor SSH log analyzer: improved structure, added colors and error handling

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -1,33 +1,68 @@
 #!/bin/bash
 
-LOGFILE="/var/log/secure"
+LOG_FILE="/var/log/secure"
+TOP_LIMIT=5
+BRUTE_FORCE_THRESHOLD=5
 
-if [ ! -f "$LOGFILE" ]; then
-	echo "Log file not found: $LOGFILE"
+GREEN="\e[32m"
+RED="\e[31m"
+YELLOW="\e[33m"
+NC="\e[0m"
+
+if [ ! -f "$LOG_FILE" ]; then
+	echo "Log file not found: $LOG_FILE"
 	exit 1
 fi
 
-echo "===SSH LOGIN REPORT=="
-echo "Report generated: $(date)"
-echo "Log file: $LOGFILE"
+echo ""
+echo "============================================================"
+echo "SSH log analyzer report $(date)"
+echo "============================================================"
+echo "Log file: $LOG_FILE"
 echo ""
 
-echo "Successful SSH logins:"
-sudo grep "sshd.*Accepted password" "$LOGFILE" | wc -l
+SUCCESS_COUNT=$(grep "sshd.*Accepted password" "$LOG_FILE" | wc -l)
+
+echo "===Successful SSH logins==="
+echo "Successful SSH logins: $SUCCESS_COUNT"
+
+if [ "$SUCCESS_COUNT" -gt 0 ]; then
+   echo -e "${GREEN}OK: Successful logins detected${NC}"
+else
+   echo -e "${YELLOW}INFO: No successful SSH logins found${NC}"
+fi
+echo ""
+
+FAILED_COUNT=$(grep "sshd.*Failed password" "$LOG_FILE" | wc -l)
+
+echo "===Failed SSH logins==="
+echo "Failed SSH logins: $FAILED_COUNT"
+
+if [ "$FAILED_COUNT" -gt 0 ]; then
+   echo -e  "${RED}WARNING: Failed SSH login attempts detected${NC}"
+else
+   echo -e "${GREEN}OK: No failed SSH logins found${NC}"
+fi
+
 
 echo ""
-echo "Failed SSH logins:"
-sudo grep "sshd.*Failed password" "$LOGFILE" | wc -l
-
+echo "===Successful SSH login IP adresses==="
+grep "sshd.*Accepted password for" "$LOG_FILE" \
+| awk '{for(i=1;i<=NF;i++) if($i ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) print $i}' \
+| sort \
+| uniq -c \
+| sort -nr
 echo ""
-echo "===SSH LOGIN IP ADDRESSES==="
-sudo grep "sshd.*Accepted password for" "$LOGFILE" | awk '{for(i=1;i<=NF;i++) if($i ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) print $i}' | 
-sort | uniq -c | sort -nr
 
+echo "===Potential brute force sources==="
+grep "sshd.*Failed password for" "$LOG_FILE" \
+| awk '{for(i=1;i<=NF;i++) if($i ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) print $i}' \
+| sort \
+| uniq -c \
+| awk -v threshold="$BRUTE_FORCE_THRESHOLD" '$1 >= threshold' \
+| sort -nr \
+| head -"$TOP_LIMIT"
 echo ""
-echo "===POTENTIAL BRUTE FORCE ATTACKS==="
-sudo grep "sshd.*Failed password for" "$LOGFILE" | awk '{for(i=1;i<=NF;i++) if($i ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) print $i}' | 
-sort | uniq -c | sort -nr | head -5
-
-echo ""
+echo "============================================================"
 echo "===END OF RAPORT==="
+echo "============================================================"
